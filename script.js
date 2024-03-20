14ff8eee14f86d43dcae49550cfed8100df0232d
TrueNQH <nguyenquanghuyktd@gmail.com>
2024-03-21 01:40:23 +0700

    deploy

---
+++ script.js
@@ -0,0 +1,32 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+const audio = new Audio('./audio/punch.mp3');
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
+  }
+}
+
+let isAlive = setInterval(function () {
+  // get current dino Y position
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+  // get current cactus X position
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+  // detect collision
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    // collision
+    audio.play(); 
+  }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
